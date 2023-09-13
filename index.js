e693033e755818348e757c93df14a997b13a427c
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-09-13 13:04:15 -0400

    pass tests

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  return word === word.split('').reverse().join('')
 }
 
 /* 
   Add your pseudocode here
+  //return statement should be a true or false expression
+  // word is equal to word => split into array => reversed => joined back into a string
 */
 
 /*
   Add written explanation of your solution here
+  See pseudocode above
 */
 
 // You can run `node index.js` to view these console logs
